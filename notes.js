dbc53ddeeb246d2f49dc55fe5c10831f0b07fb5f
Anton Keks <anton@azib.net>
2025-01-15 21:52:54 +0200

    get rid of jquery

--- notes.js
+++ notes.js
@@ -1,81 +1,104 @@
-var staff = $('.staff');
-populateStaff(staff.eq(0), 'c', 15);
-populateStaff(staff.eq(1), 'e', -5);
+const staff = document.querySelectorAll('.staff')
+
+populateStaff(staff[0], 'c', 15)
+populateStaff(staff[1], 'e', -5)
 
 function populateStaff(staff, note, halfTones) {
-  var halfToneBase = Math.pow(2, 1/12);
-  for (var i = -3; i < 10; i++) {
-    var freq = 110 * Math.pow(halfToneBase, halfTones);
-    freq = Math.round(freq * 100) / 100;
-    $('<div class="note"></div>').appendTo(staff)
-      .css({top: -0.095 * i + 'em', left: 3 + i / 4 * 3 + 'em'})
-      .attr('title', note).data('freq', freq);
-    if (note == 'e' || note == 'b') halfTones++; else halfTones+=2;
-    note = nextNote(note);
+  const halfToneBase = Math.pow(2, 1 / 12)
+  for (let i = -3; i < 10; i++) {
+    let freq = 110 * Math.pow(halfToneBase, halfTones)
+    freq = Math.round(freq * 100) / 100
+
+    const noteDiv = document.createElement('div')
+    noteDiv.className = 'note'
+    noteDiv.style.top = -0.095 * i + 'em'
+    noteDiv.style.left = 3 + (i / 4) * 3 + 'em'
+    noteDiv.setAttribute('title', note)
+    noteDiv.dataset.freq = freq
+    staff.appendChild(noteDiv)
+
+    if (note === 'e' || note === 'b') {
+      halfTones++
+    } else {
+      halfTones += 2
+    }
+    note = nextNote(note)
   }
 }
 
 function nextNote(note) {
-  note = String.fromCharCode(note.charCodeAt(0) + 1);
-  if (note == 'h') note = 'a';
-  return note;
+  note = String.fromCharCode(note.charCodeAt(0) + 1)
+  if (note === 'h') note = 'a'
+  return note
 }
 
-var audio = new (window.AudioContext || window.webkitAudioContext)();
-var players = [];
+const audio = new (window.AudioContext || window.webkitAudioContext)()
+const players = []
 
-var notes = $('.staff .note');
-notes.on('click', function() {
-  new Player($(this)).stopAfter(1);
-});
-notes.on('touchstart', function(e) {
-  var note = $(this);
-  players[note.index()] = new Player(note);
-  return false;
-});
-notes.on('touchend', function() {
-  var note = $(this);
-  var i = note.index();
-  players[i].stop();
-  delete players[i];
-  return false;
-});
+const notes = document.querySelectorAll('.staff .note')
+notes.forEach((note, index) => {
+  note.addEventListener('click', () => {
+    new Player(note).stopAfter(1)
+  })
 
-$('.notes > span').on('click', function() {
-  $('.staff .note[title=' + this.textContent + ']').trigger('click');
-});
+  note.addEventListener('touchstart', e => {
+    players[index] = new Player(note)
+    e.preventDefault()
+  })
+
+  note.addEventListener('touchend', () => {
+    if (players[index]) {
+      players[index].stop()
+      delete players[index]
+    }
+  })
+})
+
+const noteButtons = document.querySelectorAll('.notes > span')
+noteButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const matchingNotes = document.querySelectorAll(`.staff .note[title="${button.textContent}"]`)
+    matchingNotes.forEach(note => {
+      note.click()
+    })
+  })
+})
 
 function Player(note) {
-  var pub = this;
-  pub.freq = note.data('freq');
-  note.addClass('active');
-  var startTime = audio.currentTime;
+  const pub = this
+  pub.freq = note.dataset.freq
+  note.classList.add('active')
+  const startTime = audio.currentTime
 
-  var gain = audio.createGain();
-  gain.gain.setValueAtTime(1, startTime);
-  gain.gain.exponentialRampToValueAtTime(0.5, startTime + 1);
-  gain.connect(audio.destination);
+  const gain = audio.createGain()
+  gain.gain.setValueAtTime(1, startTime)
+  gain.gain.exponentialRampToValueAtTime(0.5, startTime + 1)
+  gain.connect(audio.destination)
 
-  var oscillator = audio.createOscillator();
-  oscillator.connect(gain);
-  oscillator.frequency.value = pub.freq;
-  oscillator.type = 'square';
-  oscillator.start();
+  const oscillator = audio.createOscillator()
+  oscillator.connect(gain)
+  oscillator.frequency.value = pub.freq
+  oscillator.type = 'square'
+  oscillator.start()
 
-  $('.playing').show().text(note[0].title + ' = ' + pub.freq + 'Hz').fadeOut(1000);
+  const playingDisplay = document.querySelector('.playing')
+  playingDisplay.style.display = 'block'
+  playingDisplay.textContent = note.title + ' = ' + pub.freq + 'Hz'
+  setTimeout(() => {
+    playingDisplay.style.display = 'none'
+  }, 1000)
 
   this.stop = function() {
-    var elapsed = audio.currentTime - startTime;
+    const elapsed = audio.currentTime - startTime
     if (elapsed < 1) {
-      pub.stopAfter(1 - elapsed);
+      pub.stopAfter(1 - elapsed)
+    } else {
+      oscillator.stop()
+      note.classList.remove('active')
     }
-    else {
-      oscillator.stop();
-      note.removeClass('active');
-    }
-  };
+  }
 
   this.stopAfter = function(sec) {
-    setTimeout(pub.stop, sec * 1000);
-  };
+    setTimeout(pub.stop, sec * 1000)
+  }
 }
